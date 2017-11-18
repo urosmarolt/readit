f443f36240fefbc39133aa282afc4cc101a79d52
Uros Marolt <uros.marolt@mac0061.local>
2017-11-18 12:34:25 +0000

    Add list of authors, author and book page

--- readit/urls.py
+++ readit/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from books.views import list_books
 from django.conf import settings
 
+from books.views import AuthorDetail, AuthorList, BookDetail, list_books
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', list_books, name="books"),
+    url(r'^authors/$', AuthorList.as_view(), name="authors"),
+    url(r'^book/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name="book-detail"),
+    url(r'^author/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name="author-detail"),
 ]
 
 if 'debug_toolbar' in settings.INSTALLED_APPS:
