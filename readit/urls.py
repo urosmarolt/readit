6af597380d55f999a71ba8fab9f59e7e201f8aa5
Uros Marolt <uros.marolt@mac0061.local>
2017-11-19 21:13:06 +0000

    Add login and logout views

--- readit/urls.py
+++ readit/urls.py
@@ -17,10 +17,18 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from books.views import AuthorDetail, AuthorList, BookDetail, list_books, ReviewList, review_book, CreateAuthor
 
 urlpatterns = [
+    #Auth
+    url(r'^logout/$', auth_views.logout,{'next_page': 'books'}, name="logout"),
+    url(r'^login/$', auth_views.login,{'template_name': 'login.html'}, name="login"),
+
+    #Admin
     url(r'^admin/', admin.site.urls),
+
+    #Custom
     url(r'^$', list_books, name="books"),
     url(r'^authors/$', AuthorList.as_view(), name="authors"),
     url(r'^book/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name="book-detail"),
