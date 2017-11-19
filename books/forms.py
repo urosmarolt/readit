f453055b8c101f3b15c8516a9a7b055075b5fbed
Uros Marolt <uros.marolt@mac0061.local>
2017-11-19 19:19:53 +0000

    Add reviewed_by field to Book

--- books/forms.py
+++ books/forms.py
@@ -24,7 +24,7 @@ class ReviewForm(forms.Form):
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'authors']
+        fields = ['title', 'authors', 'reviewed_by']
 
     def clean(self):
         #super the clean method to maintain validation and error messages
