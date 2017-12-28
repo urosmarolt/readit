0aef839d29bc26182083a9efa4a5566b123aed77
Uros Marolt <uros.marolt@mac0061.local>
2017-12-28 11:12:20 +0000

    Update setting and requirements to support live production and deploy to Heroku

--- readit/settings.py
+++ readit/settings.py
@@ -109,6 +109,12 @@ elif DJANGO_MODE == 'staging':
             'PORT': os.getenv('DB_PORT', '5432'),
         }
     }
+elif DJANGO_MODE == 'production':
+    import dj_database_url
+    #Handles DATABASE_URL env table
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
 
 
 # Password validation
@@ -148,6 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'readit', 'static'),
 )
