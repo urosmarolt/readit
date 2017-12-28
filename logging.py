4575b05473bd8b02d0119b396873c0b09041d315
Uros Marolt <uros.marolt@mac0061.local>
2017-12-28 12:20:07 +0000

    Commented out looging.py

--- logging.py
+++ logging.py
@@ -5,7 +5,7 @@
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-
+"""
 LOGGING = {
 	'version': 1,
 	'disable_existing_loggers': False,
@@ -29,3 +29,4 @@ LOGGING = {
 		},
 	}
 }
+"""
